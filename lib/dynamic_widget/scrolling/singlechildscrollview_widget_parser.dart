d75ebf1e7a1ce7ebbd8eb7f566bd3a7f3771029d
Purushotam Kumar <kumarsiddy@gmail.com>
2020-07-25 22:21:22 +0530

    singlechildscrollview parser added

---
+++ lib/dynamic_widget/scrolling/singlechildscrollview_widget_parser.dart
@@ -0,0 +1,29 @@
+import 'package:dynamic_widget/dynamic_widget.dart';
+import 'package:flutter/material.dart';
+
+class SingleChildScrollViewParser extends WidgetParser {
+  @override
+  Widget parse(Map<String, dynamic> map, BuildContext buildContext,
+      ClickEventListener listener,
+      {GlobalKey<State<StatefulWidget>> stateKey}) {
+    var scrollDirection = Axis.vertical;
+    if (map.containsKey("scrollDirection") &&
+        "horizontal" == map["scrollDirection"]) {
+      scrollDirection = Axis.horizontal;
+    }
+
+    Map<String, dynamic> childMap = map['child'];
+    Widget child = childMap == null
+        ? null
+        : DynamicWidgetBuilder.buildFromMap(childMap, buildContext, listener,
+            stateKey: stateKey);
+
+    return SingleChildScrollView(
+      child: child,
+      scrollDirection: scrollDirection,
+    );
+  }
+
+  @override
+  String get widgetName => 'SingleChildScrollView';
+}
